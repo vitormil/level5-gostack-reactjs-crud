0e02a34be7dbf846bf49d013aebed01fd9f29129
Vitor Oliveira <vitormil@gmail.com>
2020-10-07 18:15:22 -0300

    Challenge 10 implementation

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { FiEdit3, FiTrash } from 'react-icons/fi';
+import api from '../../services/api';
 
 import { Container } from './styles';
 
@@ -27,11 +28,18 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    const newAvailableStatus = !isAvailable;
+
+    await api.put(`/foods/${food.id}`, {
+      ...food,
+      available: newAvailableStatus,
+    });
+
+    setIsAvailable(newAvailableStatus);
   }
 
   function setEditingFood(): void {
-    // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+    handleEditFood(food);
   }
 
   return (
